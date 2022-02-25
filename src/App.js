72b07bffeb03427ca0c333d402aa6ee6467d7ce0
Muhammad Mustapha Sesay <muhammadsesay8@gmail.com>
2022-02-25 16:21:22 +0000

    Add: styling for reservation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Reserve from './Components/Pages/Reserve';
 import MyReservations from './Components/Pages/MyReservations';
 import AddCar from './Components/Pages/AddCar';
 import DeleteCar from './Components/Pages/DeleteCar';
+import carSampleData from './data/sample_data';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Car />} />
           <Route path="/Reserve" element={<Reserve />} />
-          <Route path="/MyReservations" element={<MyReservations />} />
+          <Route path="/MyReservations" element={<MyReservations props={carSampleData} />} />
           <Route path="/AddCar" element={<AddCar />} />
           <Route path="/DeleteCar" element={<DeleteCar />} />
         </Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryGreen: '#94C413',
+      },
+    },
   },
   plugins: [],
 }
