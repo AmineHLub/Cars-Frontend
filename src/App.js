8082ebf9efdde96c075ef600565cb76dcabab351
Muhammad Mustapha Sesay <muhammadsesay8@gmail.com>
2022-02-26 05:39:39 +0000

    Add button to hide and show the car details

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Reserve from './Components/Pages/Reserve';
 import MyReservations from './Components/Pages/MyReservations';
 import AddCar from './Components/Pages/AddCar';
 import DeleteCar from './Components/Pages/DeleteCar';
-import carSampleData from './data/sample_data';
 import backIcon from './assets/images/back-icon.svg';
 
 function App() {
@@ -17,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Car />} />
           <Route path="/Reserve" element={<Reserve />} />
-          <Route path="/MyReservations" element={<MyReservations props={carSampleData} />} />
+          <Route path="/MyReservations" element={<MyReservations />} />
           <Route path="/AddCar" element={<AddCar />} />
           <Route path="/DeleteCar" element={<DeleteCar />} />
         </Routes>
