7b29e6246882a5455e7137b1dda9165c6ceea1d8
sheylaPozo <shiggipoke@gmail.com>
2022-02-27 19:00:07 -0500

    delete component

--- src/Components/Pages/DeleteCar.js
+++ src/Components/Pages/DeleteCar.js
@@ -1,5 +1,85 @@
-const DeleteCar = () => (
-  <div>Delete Car</div>
-);
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCar, fetchCars } from '../../store/action/carActions';
+import style from './Delete.scss';
+
+const DeleteCar = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.userReducer);
+
+  const Store = useSelector((state) => state.Reducer.cars);
+  const [cars, setCars] = useState(Store.filter((car) => car.user_id === user.user_id));
+
+  useEffect(() => {
+    dispatch(fetchCars());
+    const userCars = carStore.filter((car) => car.user_id === user.user_id);
+    setCars(userCars);
+  }, []);
+
+  const deleteCar = (userId, id) => {
+    dispatch(removeCar(userId, id));
+    setCars((state) => state.filter((car) => car.id !== id));
+  };
+
+  const carList = cars.map((car) => (
+    <li key={car.id} className={style.card}>
+      <img src={car.image} alt={car.title} className={style.image} />
+      <div className={style.car_info}>
+        <h1>{car.title}</h1>
+        <p className={style.info}>{car.car_description}</p>
+        <div className={style.info}>
+          <span> Location:</span>
+
+          <span>
+            {car.location}
+          </span>
+        </div>
+        <div className={style.info}>
+          <span> Availability:</span>
+
+          <span>
+            {car.availability}
+              &nbsp;month
+          </span>
+        </div>
+        <div className={style.info}>
+          <span>Price:</span>
+
+          <span>
+            $
+            {car.price}
+          </span>
+        </div>
+        <div className={style.info}>
+          <span> Discount:</span>
+
+          <span>
+            {car.discount}
+            %
+          </span>
+        </div>
+      </div>
+      <button type="button" onClick={() => deletecar(car.user_id, car.id)} className={style.btn_danger}>Delete</button>
+    </li>
+  ));
+
+  return (
+    <div className={style.container}>
+      <h1 className={style.page_title}>
+        Remove car
+      </h1>
+      {cars.length > 0 ? (
+        <ul className={style.list}>
+          {carList}
+        </ul>
+      )
+        : (
+          <p>
+            You don't have any reserved cars.
+          </p>
+        )}
+    </div>
+  );
+};
 
 export default DeleteCar;
---
+++ src/Store/action/carActions.js
@@ -0,0 +1,30 @@
+import cars from '../../api/cars';
+import ActionTypes from '../types/action-types';
+
+export const receiveCars = (cars) => ({
+  type: ActionTypes.RECEIVE_CARS,
+  cars,
+});
+
+export const fetchCars = () => (dispatch) => {
+  cars.fetchCars().then((cars) => {
+    dispatch(receiveCars(cars));
+    return cars;
+  });
+};
+
+export const removeCar = (userId, id) => (dispatch) => {
+  cars.removeCar(userId, id).then((response) => {
+    dispatch(fetchCars());
+    return response;
+  });
+};
+
+export const addCar = (car) => (dispatch) => {
+  cars.addCar(car).then((response) => {
+    if (response.id) {
+      dispatch(fetchCars());
+    }
+    return response;
+  });
+};
