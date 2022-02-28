44f8911b77985a6ee35068ae20ad518872d1c3a3
sheylaPozo <shiggipoke@gmail.com>
2022-02-27 20:50:36 -0500

    fix indentation

--- src/Store/types/types.js
+++ src/Store/types/types.js
@@ -1,10 +1,10 @@
 const ActionTypes = {
-    FETCH_CARS: 'FETCH_CARS',
-    RECEIVE_CARS: 'RECEIVE_CARS',
-    SHOW_CAR: 'SHOW_CAR',
-    REMOVE_SHOW_CAR: 'REMOVE_SHOW_CAR',
-    ADD_CAR: 'ADD_CAR',
-    DELETE_CAR: 'DELETE_CAR',
-  };
+  FETCH_CARS: 'FETCH_CARS',
+  RECEIVE_CARS: 'RECEIVE_CARS',
+  SHOW_CAR: 'SHOW_CAR',
+  REMOVE_SHOW_CAR: 'REMOVE_SHOW_CAR',
+  ADD_CAR: 'ADD_CAR',
+  DELETE_CAR: 'DELETE_CAR',
+};
   
-  export default ActionTypes;
+export default ActionTypes;
