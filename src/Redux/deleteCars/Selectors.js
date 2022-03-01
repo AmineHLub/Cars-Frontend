ecd292307ae9addb60ef575447e6bc4743933fd1
sheylaPozo <shiggipoke@gmail.com>
2022-03-01 04:01:34 -0500

    fix linter errors

--- src/Redux/deleteCars/Selectors.js
+++ src/Redux/deleteCars/Selectors.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import baseUrl from '../State/baseUrl'
 
 // eslint-disable-next-line import/prefer-default-export
-/*eslint semi: [2, "never"]*/
+/*eslint semi: ["error", "never"]*/
 export const isConnect = async () => {
   let answer = false;
   let token = '';
