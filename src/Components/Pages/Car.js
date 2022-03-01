1ae4fe204de11e6bd0b4776ae7b4717631338f5b
AmineHLub <Amino.bhl@gmail.com>
2022-03-01 02:27:37 +0100

    add state for go back button

--- src/Components/Pages/Car.js
+++ src/Components/Pages/Car.js
@@ -1,14 +1,29 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { fetchCars } from '../../Redux/State/Car';
+import '../../assets/car-page/cars.css';
+import Detail from './Detail';
 
 const Car = () => {
+  const [selectedCar, setSelectedCar] = useState('');
+
+  const carObj = useSelector((state) => state.carReducer);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchCars());
   }, []);
   return (
-    <div>Car</div>
+    <div className="cars-container d-flex">
+      {!selectedCar
+        ? carObj.map((car) => (
+          <div key={car.id} role="button" tabIndex="0" onKeyPress={() => null} onClick={() => setSelectedCar(car)} className="car-card">
+            <img src={car.image} alt="car-logo" />
+            <h2>{car.name}</h2>
+            <p>{car.description}</p>
+          </div>
+        ))
+        : <Detail selectedCar={selectedCar} setSelectedCar={setSelectedCar} />}
+    </div>
   );
 };
 
