1f93b1dcb6209e910016b8da20a17cefb716f705
Muhammad Mustapha Sesay <muhammadsesay8@gmail.com>
2022-03-01 23:33:49 +0000

    Add: icons for the reserve form

--- src/Components/Pages/Reserve.js
+++ src/Components/Pages/Reserve.js
@@ -2,16 +2,28 @@ import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import { useForm } from 'react-hook-form';
 import 'react-datepicker/dist/react-datepicker.css';
-// import ReservationForm from '../forms/ReservationForm';
+import Select from 'react-select';
+import carIcon from '../../assets/images/car-icon.svg';
+import pendingIcon from '../../assets/images/pending-icon.svg';
+import calendarIcon from '../../assets/images/calendar-icon.svg';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
 
 const Reserve = () => {
-  const { handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
   const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedCar, setSelectedCar] = useState(null);
 
   const onSubmit = (data) => {
     const reserveData = {
-      data,
+      car_id: selectedCar,
+      user_id: 1,
       start_date: selectedDate,
+      duration: data.duration,
     };
 
     console.log(reserveData);
@@ -35,17 +47,23 @@ const Reserve = () => {
             onSubmit={handleSubmit(onSubmit)}
             className="bg-white border border-white rounded-md"
           >
+            <div className="border border-blue-300 flex items-center">
+              <div className="w-10">
+                <div className="flex left-0 w-10 items-center pl-3 pointer-events-none">
+                  <img src={carIcon} alt="carIcon" className="w-5 h-5 text-gray-500" />
+                </div>
+              </div>
+              <Select
+                onChange={(selected) => setSelectedCar(selected.value)}
+                options={options}
+                placeholder="Select a car"
+                className="w-full h-full"
+              />
+            </div>
             <div className="border border-blue-300 flex items-center justify-center w-full">
               <div className="w-10">
                 <div className="flex left-0 w-10 items-center pl-3 pointer-events-none">
-                  <svg
-                    className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
-                  </svg>
+                  <img src={calendarIcon} alt="carIcon" className="w-5 h-5 text-gray-500" />
                 </div>
               </div>
               <div className="w-full">
@@ -54,13 +72,23 @@ const Reserve = () => {
                   selected={selectedDate}
                   onChange={(date) => setSelectedDate(date)}
                   minDate={new Date()}
-                  className="h-14 w-full"
-                  placeholder="Select date"
+                  className="h-12 w-full"
+                  placeholderText="Select date"
                 />
               </div>
             </div>
             <div className="border border-blue-300 flex items-center">
-              <input type="text" placeholder="Duration" className="text-gray-800 pl-3 h-14 w-full" />
+              <div className="w-10">
+                <div className="flex left-0 w-10 items-center pl-3 pointer-events-none">
+                  <img src={pendingIcon} alt="carIcon" className="w-5 h-5 text-gray-500" />
+                </div>
+              </div>
+              <input
+                type="text"
+                placeholder="Duration"
+                className="text-gray-800 pl-3 h-12 w-full"
+                {...register('duration')}
+              />
             </div>
             <div className="w-full h-auto border border-blue-300 flex items-center justify-center pt-10 pb-5">
               <button type="submit" className="bg-blue-500 p-3 rounded-full w-full mx-14 text-white font-semibold cursor-pointer">Reserve</button>
