ae819df13392a01b14bedb1b328c861c051bbcd7
AmineHLub <Amino.bhl@gmail.com>
2022-03-02 18:01:48 +0100

    fix minor linter errors and refactor deleting to work with the new adding feature

--- src/Redux/State/Car.js
+++ src/Redux/State/Car.js
@@ -2,14 +2,7 @@ import Axios from 'axios';
 import baseUrl from './baseUrl';
 
 const GET_CARS = 'cars/GET_CARS';
-const MARK_DELETED = 'cars/MARK_DELETED';
 
-export const markDeleted = (payload) => ({
-  type: MARK_DELETED,
-  payload,
-});
-
-// dispatch modified const cars, added property toShow: true by default
 export const fetchCars = () => async (dispatch) => {
   const response = await Axios.get(`${baseUrl}/cars`);
   const fetchedData = response.data;
@@ -26,14 +19,6 @@ const carReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_CARS:
       return action.payload;
-    case MARK_DELETED: {
-      const updatedState = state.map((car) => {
-        if (car.id !== action.payload.id) { return car; }
-
-        return { ...car, toShow: false };
-      });
-      return [...updatedState];
-    }
     default:
       return state;
   }
