2df379cfeb73c967d404f83c5a10ea2531fc2358
Muhammad Mustapha Sesay <muhammadsesay8@gmail.com>
2022-03-02 01:03:31 +0000

    Add: reservation form redux reducer

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,8 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import Logger from 'redux-logger';
 import carReducer from './State/Car';
+import reservationReducer from './Reservation/Reservation';
 
-const reducer = combineReducers({ carReducer });
+const reducer = combineReducers({ carReducer, reservationReducer });
 
 const store = createStore(
   reducer,
