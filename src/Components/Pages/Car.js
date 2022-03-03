06d2ecf6230a34e7c027a7fa565383b04402c3f3
AmineHLub <Amino.bhl@gmail.com>
2022-03-03 13:02:23 +0100

    Reset car reducer

--- src/Components/Pages/Car.js
+++ src/Components/Pages/Car.js
@@ -8,26 +8,14 @@ const Car = () => {
   const [selectedCar, setSelectedCar] = useState('');
 
   const carObj = useSelector((state) => state.carReducer);
-
-  // filters only cars where property 'toShow' is true
-  const filteredCars = carObj.filter(
-    (car) => car.toShow,
-  );
-
-  // every time a user used to go to the CARS page, it fetched again from API,
-  // meaning it restored also the deleted items, that's why I had to
-  // added line 22, so it fetches only when the list is empty at the beginning.
   const dispatch = useDispatch();
   useEffect(() => {
-    if (carObj.length < 1) {
-      dispatch(fetchCars());
-    }
+    dispatch(fetchCars());
   }, []);
-
   return (
     <div className="cars-container d-flex">
       {!selectedCar
-        ? filteredCars.map((car) => (
+        ? carObj.map((car) => (
           <div key={car.id} role="button" tabIndex="0" onKeyPress={() => null} onClick={() => setSelectedCar(car)} className="car-card">
             <img src={car.image} alt="car-logo" />
             <h2>{car.name}</h2>
